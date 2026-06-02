296016241a480195b28e4040ae23dd68da5c3c7d
okurorinto <okurorinto@gmail.com>
2026-06-02 18:25:37 +0900

    CSPヘッダー削除・imagesConfig追加

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "img-src 'self' data: https: blob:;",
-          },
-        ],
-      },
-    ]
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+    ],
   },
 };
 
